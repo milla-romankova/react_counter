7cee9330f257386cc6cc331c7bd798a0e03ed5eb
milla-romankova <milla-romankova@gmail.com>
2020-10-28 12:23:55 +0200

    Updates

---
+++ precache-manifest.1b754c76b26ac685d0655639e00edc12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a384f50268b06d8cbd5b40eaf5b0159",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09fff10625fbea93af82",
+    "url": "./static/css/main.dc6258e3.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09fff10625fbea93af82",
+    "url": "./static/js/main.9d73d9d4.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
